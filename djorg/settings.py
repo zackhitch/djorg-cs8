30db717c5c18574f7cd171fbcdcbf332545f7b03
zackhitch <zackhitch@users.noreply.github.com>
2018-06-29 11:09:16 -0700

    added static root to settings.py - Z H

--- djorg/settings.py
+++ djorg/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
