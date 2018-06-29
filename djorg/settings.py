aaef83b4442ce6fad008b42dc2b40b0c1ecb7e50
zackhitch <zackhitch@users.noreply.github.com>
2018-06-29 12:33:15 -0700

    heroku debugging

--- djorg/settings.py
+++ djorg/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 REST_FRAMEWORK = {
